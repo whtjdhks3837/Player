8016aa8d74e325fcf7ddb1e3c3530b56eb2aded3
BaeJang <qowod77@naver.com>
2020-07-06 01:47:13 +0900

    이미지, 비디오 추가

--- dynamic_media_view/src/main/java/baejang/dynamic_media_view/data/MediaList.kt
+++ dynamic_media_view/src/main/java/baejang/dynamic_media_view/data/MediaList.kt
@@ -1,10 +1,10 @@
 package baejang.dynamic_media_view.data
 
-const val hlsVideoUrl1 =
+const val hlsVideoUrl =
     "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-const val hlsVideoUrl2 = "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8"
-const val hlsVideoUrl3 = "http://www.streambox.fr/playlists/test_001/stream.m3u8" // 404 video
-const val hlsVideoUrl4 = "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8" // 404 video
-const val hlsVideoUrl5 = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8"
-
-const val introAsset = "asset:///intro.mp4"
+const val mp4VideoUrl =
+    "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+const val webmVideoUrl =
+    "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-webm-file.webm"
+const val assetApiVideo =
+    "asset:///sample-mp4-file.mp4"
